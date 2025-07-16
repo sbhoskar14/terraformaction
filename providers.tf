cf04baedabaf27e6ee27103752da287f41297aa7
Shivam Bhoskar <E042125@adgroup.michelin.com>
2025-07-16 12:14:56 +0530

    New commit

--- providers.tf
+++ providers.tf
@@ -1,12 +1,12 @@
-# provider "azurerm" {
-#   tenant_id       = var.tenant_id
-#   subscription_id = var.sub_id
-#   client_id       = var.sp_id
-#   client_secret   = var.sp_secret
-#   features {
+provider "azurerm" {
+  tenant_id       = var.tenant_id
+  subscription_id = var.sub_id
+  client_id       = var.sp_id
+  client_secret   = var.sp_secret
+  features {
 
-#   }
-# }
+  }
+}
 
 terraform {
  backend "azurerm" {
